4bcf081459107a7cd5283128bbecdb91192ee26f
The bluetouch <bogusz28@wp.pl>
2021-11-19 19:33:39 +0100

    Initial commit

---
+++ src/test/java/pl/restaurantmanagementsystem/RMSmock/RmsMockApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.restaurantmanagementsystem.RMSmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
